2a686b5242728439fc2f1e9087b18bdc38c58cc7
Aashi-code77 <pandaoutlier@gmail.com>
2025-04-02 19:55:46 +0530

    Update vs_sortcut_imp.py

--- vs_sortcut_imp.py
+++ vs_sortcut_imp.py
@@ -1,3 +1,5 @@
 # if hme code upar niche then simple click on code then press alt and use up and down key
 # if hme code ka duplicate krna ho multiple times then click on code then shift + Alt + downkey
 #  if file ko undo krna ho to use ctrl + z
+# jb hme ek hi trah ke name ko change krna ho aur uske jagah dusre name  dalna ho to hme -- right click kr ke -- change all occurence / ctrl + F2 then press backspace and change it  
+# and if () ")" end me dana ho then press end key and then press shift + 0 
